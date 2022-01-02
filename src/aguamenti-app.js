782d985040a66f8806015cbd2acec5fa8bba9153
Anindya Mitra <55695557+akm15machine@users.noreply.github.com>
2022-01-02 23:54:30 +0530

    main route started

--- src/aguamenti-app.js
+++ src/aguamenti-app.js
@@ -6,15 +6,14 @@ require('dotenv').config();
 
 const port = Number(process.env.PORT || 3000);
 const uri = String(process.env.MONGO_URI);
+const startTime = new Date();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', async(req, res)=> {
-    console.log(req.query)
-    res.send("gg")
-})
+const mainRouter = require('./routes/main.route')
+app.use('/', mainRouter)
 
 app.listen(port, ()=>{
     console.log(`Server running at: http://localhost:${port}`)
---
+++ src/routes/main.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { RootController } = require("../controllers/main.controller");
+const router = express.Router();
+
+router.get('/', RootController);
+
+module.exports = router;
