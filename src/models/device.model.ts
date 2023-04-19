9faf5831b0bfedd88333ce6783a5050b247d0fd9
Anindya Mitra <anindya15l12@gmail.com>
2023-04-19 20:18:25 +0530

    schedule model added

--- src/models/device.model.ts
+++ src/models/device.model.ts
@@ -3,7 +3,6 @@ import { Document, Model, model, Schema, Types } from "mongoose";
 export type DeviceType = "switch" | "slider" | "pump" | "tank_level";
 export const DeviceTypes: string[] = ["switch", "slider", "pump", "tank_level"];
 
-let device: DeviceType = "switch"
 
 interface DeviceInterface extends Document {
     name: string,
@@ -11,10 +10,9 @@ interface DeviceInterface extends Document {
     state?: boolean,
     value?: any, //TODO: check if any restrictions should be applied
     online: boolean,
-    schedules: [Schedule]
+    // schedules: [Schedule]
     house_id?: Types.ObjectId,
     device_type?: DeviceType,
-    linked_chip_id?: string
 };
 
 const DeviceSchema: Schema<DeviceInterface> = new Schema(
@@ -29,7 +27,6 @@ const DeviceSchema: Schema<DeviceInterface> = new Schema(
         state: { type: Boolean },
         value: { type: Schema.Types.Mixed },
         house_id: { type: Schema.Types.ObjectId, ref: 'House' },
-        linked_chip_id: { type: String }
     },
     {
         timestamps: true
@@ -39,14 +36,5 @@ const DeviceSchema: Schema<DeviceInterface> = new Schema(
 
 const Device: Model<DeviceInterface> = model("Device", DeviceSchema);
 
-export interface Schedule{
-    schedule_type: ScheduleType,
-    trigger_type: TriggerType,
-    repeat_on: [WeekDay],
-    end_at: Date    //js built-in date class
-}; 
 
 export default Device;
-export type ScheduleType = "on" | "off" | "dim" | "and" | "or";
-export type TriggerType = "timing" | "device";
-export type WeekDay = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday"; // note: I am sure there are alternative to this in-built.
---
+++ src/models/schedule.model.ts
@@ -0,0 +1,52 @@
+import { Document, Model, model, Schema, Types } from "mongoose";
+
+export type ScheduleType = "on" | "off" | "dim" | "and" | "or";
+export const ScheduleTypes: string[] = ["on", "off", "dim", "and", "or"];
+
+export type TriggerType = "timing" | "action";
+export const TriggerTypes: string[] = ["timing", "action"];
+
+
+export type WeekDay = "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
+export const WeekDayList = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+export const WeekDays: { [key: number]: WeekDay } = Object.freeze({
+    0: 'mon',
+    1: 'tue',
+    2: 'wed',
+    3: 'thu',
+    4: 'fri',
+    5: 'sat',
+    6: 'sun'
+});
+
+interface ScheduleInterface extends Document {
+    name: string,
+    chip_id: string,
+    linked_chip_id?: string,
+    schedule_type: ScheduleType,
+    trigger_type?: TriggerType,
+    repeat_on?: [WeekDay],
+    end_at: Date,    //js built-in date class
+};
+
+
+const ScheduleSchema: Schema<ScheduleInterface> = new Schema(
+    {
+        name: { type: String, required: true },
+        chip_id: { type: String, required: true },
+        linked_chip_id: { type: String },
+        schedule_type: { type: String, enum: ScheduleTypes },
+        trigger_type: { type: String, enum: TriggerTypes, default: String(TriggerTypes[0]) },
+        repeat_on: { type: [String], enum: WeekDays },
+        end_at: { type: Date }
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+const Schedule: Model<ScheduleInterface> = model("Schedule", ScheduleSchema);
+
+export default Schedule;
