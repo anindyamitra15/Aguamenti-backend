58adb5f9639f017909839e97612594f0dbd37307
Anubhav Dutta <anubhavd666@gmail.com>
2023-05-02 15:51:40 +0530

    Routes added

--- src/routes/schedule.route.ts
+++ src/routes/schedule.route.ts
@@ -5,9 +5,9 @@ import AuthorizeUserFromToken from "../middlewares/tokenauth.middleware";
 const ScheduleRouter: Router = Router();
 
 ScheduleRouter.post("/new", AuthorizeUserFromToken, ScheduleController.CreateSchedule);
-ScheduleRouter.get("/listusingUID", AuthorizeUserFromToken, ScheduleController.ListScheduleUnderUser);
-ScheduleRouter.get("/listusingDID", AuthorizeUserFromToken, ScheduleController.ListScheduleUnderDevice);
-ScheduleRouter.get("/listusingLDID", AuthorizeUserFromToken, ScheduleController.ListScheduleUnderLinkedDevice);
+ScheduleRouter.get("/listUID", AuthorizeUserFromToken, ScheduleController.ListScheduleUnderUser);
+ScheduleRouter.get("/listDID", AuthorizeUserFromToken, ScheduleController.ListScheduleUnderDevice);
+ScheduleRouter.get("/listLDID", AuthorizeUserFromToken, ScheduleController.ListScheduleUnderLinkedDevice);
 ScheduleRouter.delete("/remove", AuthorizeUserFromToken, ScheduleController.DeleteSchedule);
 ScheduleRouter.put("/edit", AuthorizeUserFromToken, ScheduleController.EditSchedule);
 
